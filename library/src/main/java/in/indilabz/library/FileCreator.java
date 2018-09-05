0d02abb1ae2d020b6ce3f51d9326fed7adef6646
Your NameMritunjay <mjdemon211@gmail.com>
2018-09-05 12:04:32 +0530

    Preferences Updated

---
+++ library/src/main/java/in/indilabz/library/FileCreator.java
@@ -0,0 +1,17 @@
+package in.indilabz.library;
+
+
+public class FileCreator implements Data{
+
+    public static void main(String[] args) {
+
+        String PACKAGE = "in.indilabz.apphelper"; //present in manifest or build.gradle file
+        String APP_NAME = " \"AppHelper\" ";
+
+        OperationHelper.execute(PACKAGE, APP_NAME);
+
+    }
+
+
+}
+
